9a3f813623ddfba6cf513571cbc85e3d577a98e2
Senxian Z <9044823+flashsphere@users.noreply.github.com>
2026-04-03 14:32:30 +0800

    Update dependencies

--- app/src/main/java/com/flashsphere/rainwaveplayer/ui/WebView.kt
+++ app/src/main/java/com/flashsphere/rainwaveplayer/ui/WebView.kt
@@ -1,9 +1,9 @@
 package com.flashsphere.rainwaveplayer.ui
 
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.core.os.bundleOf
 import androidx.fragment.compose.AndroidFragment
 import com.flashsphere.rainwaveplayer.view.fragment.WebViewFragment
 import timber.log.Timber
@@ -15,7 +15,7 @@ fun WebView(
     pageTitle: MutableState<String>,
 ) {
     AndroidFragment<WebViewFragment>(
-        arguments = bundleOf(WebViewFragment.ARG_URL to url),
+        arguments = Bundle().also { it.putString(WebViewFragment.ARG_URL, url) },
         modifier = modifier,
         onUpdate = { fragment ->
             Timber.d("onUpdate")
