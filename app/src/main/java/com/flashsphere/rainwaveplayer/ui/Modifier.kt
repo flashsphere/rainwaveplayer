8f546a4fc4a88d2b6c03771c788ee4ce7f74e523
Senxian Z <9044823+flashsphere@users.noreply.github.com>
2026-05-18 14:06:27 +0800

    TV: Remove use of onGloballyPositioned
    Probably no longer needed 🤞

--- app/src/main/java/com/flashsphere/rainwaveplayer/ui/Modifier.kt
+++ app/src/main/java/com/flashsphere/rainwaveplayer/ui/Modifier.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.lifecycle.compose.LifecycleStartEffect
 import com.flashsphere.rainwaveplayer.ui.composition.LastFocused
 import com.flashsphere.rainwaveplayer.ui.composition.LocalLastFocused
@@ -39,13 +38,8 @@ fun Modifier.saveLastFocused(
     return this then Modifier.focusRequester(focusRequester)
         .onFocusChanged {
             if (it.hasFocus || it.isFocused || it.isCaptured) {
+                Timber.d("onFocusChanged -> %s has focus", tag)
                 lastFocused.value = LastFocused(tag = tag, shouldRequestFocus = false)
-            }
-        }
-        .onGloballyPositioned {
-            if (lastFocused.value.tag == tag && lastFocused.value.shouldRequestFocus) {
-                Timber.d("onGloballyPositioned -> requesting focus for %s", tag)
-                focusRequester.requestFocus()
             }
         }
 }
