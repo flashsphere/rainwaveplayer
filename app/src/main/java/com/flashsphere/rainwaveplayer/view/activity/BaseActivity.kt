e9e34189cc6cbba7aa2fab2c3423758e45491c11
Senxian Z <9044823+flashsphere@users.noreply.github.com>
2026-08-27 09:54:21 +0800

    Use new API for edge to edge

--- app/src/main/java/com/flashsphere/rainwaveplayer/view/activity/BaseActivity.kt
+++ app/src/main/java/com/flashsphere/rainwaveplayer/view/activity/BaseActivity.kt
@@ -1,11 +1,9 @@
 package com.flashsphere.rainwaveplayer.view.activity
 
-import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.SystemBarStyle
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import com.flashsphere.rainwaveplayer.app.RainwaveApp
@@ -29,10 +27,7 @@ abstract class BaseActivity : AppCompatActivity() {
         }
 
         Timber.tag(getSimpleClassName()).d("Creating")
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
-            navigationBarStyle = SystemBarStyle.dark(Color.argb(0x80, 0x1b, 0x1b, 0x1b))
-        )
+        WindowCompat.enableEdgeToEdge(window)
         super.onCreate(savedInstanceState)
     }
 
